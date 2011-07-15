696db6dc28b80d927fcbb04c8d652c279a50036f
Eric J. Duran <eric.duran7@gmail.com>
2011-07-15 15:21:08 -0400

    Making the JS more flexible and removing, unnecesarry task

--- views_load_more.js
+++ views_load_more.js
@@ -4,19 +4,26 @@
  * The following fields control behavior.
  * - 'display': Display the associated data in the view area.
  */
-Drupal.views_load_more = function(target, response) {
+Drupal.viewsLoadMore = function(target, response) {
   var $view = $(target);
+  var pager_class = '.pager';
+  var view_content_class = '.view-content';
+  var view_row_class = '.views-row';
 
   // Check the 'display' for data.
   if (response.status && response.display) {
     var $newView = $(response.display);
-    // We don't want to replace we want to append
-    // Also we'll want to strip out the pager data
-    $view.find('.item-list').remove();
-    $view.append($newView);
-    $view = $newView;
-    $view.parent().removeClass('views-processed');
-    Drupal.attachBehaviors($view.parent());
+    var $pager = $newView.find(pager_class);
+    // Normally views would replace here, but we're going to
+    // append instead. At the same time, we want to update
+    // the pager to the new pager and re-attached the proper
+    // behaviors for the view.
+    $view.removeClass('views-processed');
+    // Update the Pager with the new pager.
+    $view.find(pager_class).html($pager.html());
+    // Append the new content to the old view.
+    $view.find(view_content_class).append($newView.find(view_row_class));
+    Drupal.attachBehaviors($view);
   }
 
   if (response.messages) {
