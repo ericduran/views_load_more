11c24071def7306fada6cec7128679acfb2c3403
Eric J. Duran <eric.duran7@gmail.com>
2012-06-07 00:58:34 -0400

    Updating test to actually test functionality

--- test/casperjs/test1.js
+++
@@ -1,9 +0,0 @@
-casper.test.comment('Making sure our test site is loaded correctly');
-
-casper.start('http://localhost:8080/', function() {
-  this.test.assertHttpStatus(200, 'Testing site is up');
-});
-
-casper.run(function() {
-    this.test.done();
-});
