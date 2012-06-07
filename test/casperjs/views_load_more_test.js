cb6d010fea5400842b4ee7f1fc0a1d1a1a85a194
Eric J. Duran <eric.duran7@gmail.com>
2012-06-07 01:16:58 -0400

    Updating test

--- test/casperjs/views_load_more_test.js
+++ test/casperjs/views_load_more_test.js
@@ -3,17 +3,12 @@ casper.test.comment('Making sure our test site is loaded correctly');
 // Make sure the site is up
 casper.start('http://localhost:8080/test-views-load-more-pager', function() {
   this.test.assertHttpStatus(200, 'Testing site is up');
-});
+  this.test.assertTitle('test_views_load_more_pager | Site-Install', 'Title seems correct');
 
-casper.then(function() {
-  this.test.assertTitle('test_views_load_more_pager', 'Title seems correct');
-  
   this.test.assertEval(function() {
     return document.querySelectorAll('.view-test-views-load-more-pager .views-row').length == 1;
   }, 'There is only one node on the page');
-});
 
-casper.then(function() {
   this.click('ul.pager-load-more a');
   this.echo("Waiting for ajax response.");
 
